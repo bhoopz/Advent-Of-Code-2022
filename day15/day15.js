18ff1fda2529bdaf859d1cf1510703c014664a39
Dominik Filip <filu4444@gmail.com>
2022-12-28 17:04:16 +0100

    day15 done

--- day15/day15.js
+++ day15/day15.js
@@ -1,17 +1,19 @@
 const fs = require("fs");
 const array = fs
-  .readFileSync("test.txt")
+  .readFileSync("input.txt")
   .toString()
   .replaceAll("\r", "")
   .split("\n");
 
-const Y = 10;
+const Y = 2000000;
 const allIntervals = new Set([]);
 const allYBeacons = new Set([]);
-const allDiagonals = new Set([]);
+const ascLines = [];
+const descLines = [];
+
 for (line in array) {
-  sensorCords = array[line].match(/\d+/g).slice(0, 2);
-  beaconCords = array[line].match(/\d+/g).slice(2);
+  sensorCords = array[line].match(/-?\d+/g).slice(0, 2);
+  beaconCords = array[line].match(/-?\d+/g).slice(2);
   sensorCords = sensorCords.map((item) => parseInt(item));
   beaconCords = beaconCords.map((item) => parseInt(item));
   mDist =
@@ -23,16 +25,15 @@ for (line in array) {
   } else {
     if (sensorCords[1] + mDist >= Y) addIntervals();
   }
-  //   for (let x = sensorCords[0] - mDist; x <= sensorCords[0] + mDist; x++) {
-  //     for (let y = sensorCords[1] - mDist; y <= sensorCords[1] + mDist; y++) {
-  //       if (
-  //         Math.abs(x - sensorCords[0]) + Math.abs(y - sensorCords[1]) ==
-  //         mDist
-  //       ) {
-  //         allDiagonals.add(JSON.stringify([x, y]));
-  //       }
-  //     }
-  //   }
+
+  ascLines.push(
+    sensorCords[0] - sensorCords[1] - mDist,
+    sensorCords[0] - sensorCords[1] + mDist
+  );
+  descLines.push(
+    sensorCords[0] + sensorCords[1] - mDist,
+    sensorCords[0] + sensorCords[1] + mDist
+  );
 }
 
 function addIntervals() {
@@ -45,37 +46,24 @@ function addIntervals() {
   }
 }
 
-// function part2() {
-//   let maxPoint = null;
-//   let maxCount = 0;
-//   const array = Array.from(allDiagonals);
-//   const direction = [
-//     [1, 0],
-//     [0, 1],
-//     [-1, 0],
-//     [0, -1],
-//   ];
-//   //console.log(array);
-//   for (const point of array) {
-//     let count = 0;
-//     for (const dir of direction) {
-//       const newPoint = [
-//         JSON.parse(point)[0] + dir[0],
-//         JSON.parse(point)[1] + dir[1],
-//       ];
+function part2() {
+  let asc = null;
+  let desc = null;
+  for (let i = 0; i < ascLines.length; i++) {
+    for (let j = i + 1; j < ascLines.length; j++) {
+      const a = ascLines[i];
+      const b = ascLines[j];
+      if (Math.abs(a - b) == 2) asc = Math.min(a, b) + 1;
 
-//       if (allDiagonals.has(JSON.stringify(newPoint))) {
-//         console.log(newPoint);
-//         count++;
-//       }
-//     }
-//     if (count > maxCount) {
-//       maxCount = count;
-//       maxPoint = point;
-//     }
-//   }
+      const x = descLines[i];
+      const y = descLines[j];
+      if (Math.abs(x - y) == 2) desc = Math.min(x, y) + 1;
+    }
+  }
 
-//   return maxPoint;
-// }
-// console.log(part2());
+  return Math.floor((asc + desc) / 2) * 4000000 + Math.floor((desc - asc) / 2);
+}
+
 console.log(allIntervals.size - allYBeacons.size);
+
+console.log(part2());
