e0ff4b16f616a18e7e55c8f58ea3fe93fc50e10a
Dominik Filip <filu4444@gmail.com>
2023-01-05 15:19:46 +0100

    day18 done

--- day18/day18.js
+++ day18/day18.js
@@ -10,7 +10,7 @@ for (line of array) {
   set.add(JSON.stringify(line.split(",").map((x) => parseInt(x))));
 }
 let walls = set.size * 6;
-
+let part2 = 0;
 for (item of set) {
   JSON.parse(item).map((i, index) => {
     for (d of [-1, 1]) {
@@ -18,33 +18,42 @@ for (item of set) {
       if (index == 0) {
         let temp = [newI, JSON.parse(item)[1], JSON.parse(item)[2]];
         if (set.has(JSON.stringify(temp))) walls--;
+        if (bfs(temp)) part2++;
       } else if (index == 1) {
         let temp = [JSON.parse(item)[0], newI, JSON.parse(item)[2]];
         if (set.has(JSON.stringify(temp))) walls--;
+        if (bfs(temp)) part2++;
       } else {
         let temp = [JSON.parse(item)[0], JSON.parse(item)[1], newI];
         if (set.has(JSON.stringify(temp))) walls--;
+        if (bfs(temp)) part2++;
       }
     }
   });
 }
-let xd = 0;
-for (let x = 0; x <= 21; x++) {
-  for (let y = 0; y <= 21; y++) {
-    for (let z = 0; z <= 21; z++) {
-      if (
-        !set.has(JSON.stringify([x, y, z])) //&&
-        // set.has(JSON.stringify([x - 1, y, z])) &&
-        // set.has(JSON.stringify([x + 1, y, z])) &&
-        // set.has(JSON.stringify([x, y - 1, z])) &&
-        // set.has(JSON.stringify([x, y + 1, z])) &&
-        // set.has(JSON.stringify([x, y, z - 1])) &&
-        // set.has(JSON.stringify([x, y, z + 1]))
-      )
-        xd++;
-    }
+
+function bfs(start) {
+  const queue = [];
+  queue.push(start);
+  const seen = new Set();
+  while (queue.length > 0) {
+    let [x, y, z] = queue.shift();
+    if (
+      set.has(JSON.stringify([x, y, z])) ||
+      seen.has(JSON.stringify([x, y, z]))
+    )
+      continue;
+    seen.add(JSON.stringify([x, y, z]));
+    if (seen.size > 1400) return true;
+    queue.push([x - 1, y, z]);
+    queue.push([x + 1, y, z]);
+    queue.push([x, y - 1, z]);
+    queue.push([x, y + 1, z]);
+    queue.push([x, y, z - 1]);
+    queue.push([x, y, z + 1]);
   }
+  return false;
 }
 
 console.log(walls);
-console.log(walls - xd * 6);
+console.log(part2);
